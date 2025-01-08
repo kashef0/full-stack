b7e6160d0837c359c6e3b535c4383ebd97d69fbf
M.kashef <kashef.sy@gmail.com>
2025-01-08 11:01:36 +0100

    empty

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -36,7 +36,6 @@ export class TransactionService {
             beforeQty: item.quantity,
             soldQty,
             salerName,
-            
         },
     });
     await this.prisma.item.update({
