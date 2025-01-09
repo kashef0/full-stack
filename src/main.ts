073cfbe9d11d2db4278d9fd43a208a7a77a97ace
M.kashef <kashef.sy@gmail.com>
2025-01-09 13:19:12 +0100

    tilla[ta auth

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
+  app.enableCors({
+    origin: 'http://localhost:5173', 
+    methods: 'GET,POST,PUT,DELETE', 
+    allowedHeaders: 'Content-Type,Authorization', 
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
