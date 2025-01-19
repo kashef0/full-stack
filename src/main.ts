5d5f6d2885d8c16e82bbe83f0b8e0684786449f3
M.kashef <kashef.sy@gmail.com>
2025-01-19 15:09:49 +0100

    justerade cors

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,10 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
   }));
   app.enableCors({
-    origin: 'http://localhost:5173', 
+    origin: ['https://full-stack-project-20-25.netlify.app', 'http://localhost:5173'],
     methods: 'GET,POST,PUT,DELETE', 
     allowedHeaders: 'Content-Type,Authorization', 
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
